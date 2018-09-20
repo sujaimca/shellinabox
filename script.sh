f7001a2a0b0fd8e0b4d6e96e567d58d056698c82
Sujai <sujai.mca2012@gmail.com>
2018-09-20 11:10:09 +0530

    Update script.sh
    Added provision to pass runtime shell via URL

--- script.sh
+++ script.sh
@@ -2,4 +2,11 @@
 IFS='/' read -ra urls <<< "$1"
 val=${urls[-1]}
 IFS='=' read -ra conArr <<< $val
-sudo docker exec -it ${conArr[-1]} /bin/sh
+
+#ULR will have runtime shell with ',' instead of '/'
+#Replace ',' with '/'
+encoded_shell=${conArr[-1]}
+shell=$(echo $encoded_shell | tr ',' '/')
+
+#Login to container
+sudo docker exec -it "${conArr[-2]}" "$shell"
