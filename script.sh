d4713d35d0c16fa34ce56ba4db243b0d847e4414
Sujai <sujai.mca2012@gmail.com>
2018-09-20 11:38:51 +0530

    Update script.sh
    Fixed bug in getting container ID for login

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 IFS='/' read -ra urls <<< "$1"
 val=${urls[-1]}
-IFS='=' read -ra conArr <<< $val
+IFS='=' read -ra queryStringArr <<< $val
 
 #ULR will have runtime shell with ',' instead of '/'
 #Replace ',' with '/'
-encoded_shell=${conArr[-1]}
+encoded_shell=${queryStringArr[-1]}
 shell=$(echo $encoded_shell | tr ',' '/')
+
+#Get container ID from "queryStringArr[-2]"
+IFS='&' read -ra conArr <<< ${queryStringArr[-2]}
 
 #Login to container
 sudo docker exec -it "${conArr[-2]}" "$shell"
